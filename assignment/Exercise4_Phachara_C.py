969867bb4852b4b9b50ae3574b289ac71ff0adce
ModzabazeR <moddaeng.pps@gmail.com>
2019-03-14 19:07:28 +0700

    Update Exercise4_Phachara_C.py

--- assignment/Exercise4_Phachara_C.py
+++ assignment/Exercise4_Phachara_C.py
@@ -3,4 +3,8 @@ genBusiness = 74.5
 introCompSys = 80
 compProgram = 70.2
 
-print("-- Your Score ---")
+print("--- Your Score ---")
+print("Foundation English                   :", foundEng, "pts")
+print("General Business                     :", genBusiness, "pts")
+print("Introduction to Computer Systems     :", introCompSys, "pts")
+print("Computer Programming                 :", compProgram, "pts")
