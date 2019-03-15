7e5dda9d9d1e9b9b3b891bee09dc533cacb1beb6
ModzabazeR <moddaeng.pps@gmail.com>
2019-03-15 20:44:59 +0700

    Exercies 5

---
+++ assignment/Exercise5_1_Phachara_C.py
@@ -0,0 +1,9 @@
+x = int(input("Enter a number : "))
+y = int(input("Enter another number : "))
+
+print()
+
+print(x, "+", y, "=", x+y)
+print(x, "-", y, "=", x-y)
+print(x, "*", y, "=", x*y)
+print(x, "/", y, "=", int(x/y))
---
+++ assignment/Exercise5_2_Phachara_C.py
@@ -0,0 +1,7 @@
+print("Use m with second, and km with hour")
+s = int(input("Enter the distance that the car goes (m or km) : "))
+t = int(input("Enter a time that the car goes (second or hour) : "))
+v = s / t
+
+print()
+print(int(v), "km/h")
