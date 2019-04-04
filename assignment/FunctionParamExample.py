ddf8cf17596b9513f124401d65f87d0c81573f54
ModzabazeR <moddaeng.pps@gmail.com>
2019-04-04 16:08:44 +0700

    Create FunctionParamExample.py

---
+++ assignment/FunctionParamExample.py
@@ -0,0 +1,14 @@
+def addNumber(x, y):
+    print(x + y)
+def mutipleNumber(x, y):
+    print(x * y)
+def subtractNumber(x ,y):
+    print(x - y)
+def divideNumber(x, y):
+    print(x / y)
+
+
+addNumber(10, 25)
+mutipleNumber(10, 25)
+subtractNumber(10, 25)
+divideNumber(10, 25)
